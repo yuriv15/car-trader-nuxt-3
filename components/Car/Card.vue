554890ab8a36780c9154749b3798aa02e9104ad9
Yuri Gonçalves <yuri_rjx@hotmail.com>
2023-03-27 22:49:33 -0300

    update

--- components/Car/Card.vue
+++ components/Car/Card.vue
@@ -1,18 +1,33 @@
 <script setup lang="ts">
+import heartFilled from '@/assets/heartFilled.png';
+import heartOutline from '@/assets/heartOutline.png';
 import cars from '@/data/cars.json';
 
 const props = defineProps<{
     car: (typeof cars)[0];
+    favored: boolean;
+}>();
+
+const emit = defineEmits<{
+    (e: 'favor', id: number): void;
 }>();
 </script>
 
 <template>
     <div
-        class="shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]"
-        @click="navigateTo(`/car/${car.name}-${car.id}`)"
+        class="relative shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]"
     >
-        <div class="flex h-full">
-            <img :src="car.url" :alt="car.name" class="w-[300px] h-full" />
+        <img
+            class="absolute w-7 right-5 top-2 z-20"
+            :src="favored ? heartFilled : heartOutline"
+            alt="Heart Outline"
+            @click="emit('favor', car.id)"
+        />
+        <div
+            class="flex h-full"
+            @click="navigateTo(`/car/${car.name}-${car.id}`)"
+        >
+            <NuxtImg :src="car.url" :alt="car.name" class="w-[300px] h-full" />
             <div class="p-4 flex flex-col">
                 <div>
                     <h1 class="text-2xl text-blue-700">
--- components/Car/Cards.vue
+++ components/Car/Cards.vue
@@ -1,9 +1,34 @@
 <script setup lang="ts">
+import { Ref } from 'vue';
+
 const { cars } = useCars();
+
+const favorite: Ref<{ [key: number]: boolean }> = useLocalStorage(
+    'favorite',
+    {}
+);
+const handleFavorite = (id: number) => {
+    if (id in favorite.value) {
+        delete favorite.value[id];
+    } else {
+        favorite.value = {
+            ...favorite.value,
+            [id]: true,
+        };
+    }
+};
 </script>
 
 <template>
     <div class="w-full">
-        <CarCard v-for="car in cars" :key="car.id" :car="car" />
+        <ClientOnly>
+            <CarCard
+                v-for="car in cars"
+                :key="car.id"
+                :car="car"
+                @favor="handleFavorite"
+                :favored="car.id in favorite"
+            />
+        </ClientOnly>
     </div>
 </template>
