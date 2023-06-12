ce34f8e53f37299b4ea2659384cd8d6fbcb635a6
Yuri Gonçalves <yuri_rjx@hotmail.com>
2023-06-12 20:49:50 -0300

    added car make and car price filters

--- components/Car/SideBar.vue
+++ components/Car/SideBar.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+const { makes } = useCars();
 const modal = ref({
     make: false,
     location: false,
@@ -6,7 +7,22 @@ const modal = ref({
 });
 
 const city = ref('');
+const priceRange = ref({
+    min: '',
+    max: '',
+});
 const route = useRoute();
+const router = useRouter();
+
+const priceRangeText = computed(() => {
+    const minPrice = route.query.minPrice;
+    const maxPrice = route.query.maxPrice;
+
+    if (!minPrice && !maxPrice) return 'Any';
+    else if (!minPrice && maxPrice) return `< $${maxPrice}`;
+    else if (minPrice && !maxPrice) return `> $${minPrice}`;
+    else return `$${minPrice}-$${maxPrice}`;
+});
 
 function updateModal(key: keyof typeof modal.value) {
     modal.value[key] = !modal.value[key];
@@ -20,13 +36,33 @@ function onChangeLocation() {
             message: 'Invalid city format',
         });
     updateModal('location');
-    navigateTo(`/city/${city.value}/car/${route.params.make as string}`);
+    navigateTo(`/city/${city.value}/car/${route.params.make}`);
     city.value = '';
+}
+
+function onChangeMake(make: string) {
+    updateModal('make');
+    navigateTo(`/city/${route.params.city}/car/${make}`);
+}
+
+function onChangePrice() {
+    updateModal('price');
+    const { min, max } = priceRange.value;
+    if (max && min) {
+        if (min > max) return;
+    }
+    router.push({
+        query: {
+            minPrice: min,
+            maxPrice: max,
+        },
+    });
 }
 </script>
 
 <template>
     <div class="shadow border w-64 mr-10 z-30 h-[190px]">
+        <!--        LOCATION START-->
         <div class="p-5 flex justify-between relative cursor-pointer border-b">
             <h3>Location</h3>
             <h3
@@ -53,15 +89,59 @@ function onChangeLocation() {
                 </button>
             </div>
         </div>
+        <!--        LOCATION END-->
 
+        <!--        MAKE START-->
         <div class="p-5 flex justify-between relative cursor-pointer border-b">
             <h3>Make</h3>
-            <h3 class="text-blue-400 capitalize">Toyota</h3>
+            <h3 class="text-blue-400 capitalize" @click="updateModal('make')">
+                {{ route.params.make || 'Any' }}
+            </h3>
+            <div
+                class="absolute border shadow left-56 p-5 top-1 -m-1 w-[600px] flex justify-between flex-wrap bg-white"
+                v-if="modal.make"
+            >
+                <h4
+                    v-for="make in makes"
+                    :key="make"
+                    class="w-1/3"
+                    @click="onChangeMake(make)"
+                >
+                    {{ make }}
+                </h4>
+            </div>
         </div>
-
+        <!--    MAKE END-->
+        <!--PRICE START-->
         <div class="p-5 flex justify-between relative cursor-pointer border-b">
             <h3>Price</h3>
-            <h3 class="text-blue-400 capitalize">Any</h3>
+            <h3 class="text-blue-400 capitalize" @click="updateModal('price')">
+                {{ priceRangeText }}
+            </h3>
+            <div
+                class="absolute border shadow left-56 p-5 top-1 -m-1 bg-white"
+                v-if="modal.price"
+            >
+                <input
+                    class="border p-1 rounded"
+                    type="number"
+                    placeholder="Min"
+                    v-model="priceRange.min"
+                />
+                <input
+                    class="border p-1 rounded mt-2"
+                    type="number"
+                    placeholder="Max"
+                    v-model="priceRange.max"
+                />
+                <button
+                    class="bg-blue-400 w-full mt-2 rounded text-white p-1"
+                    @click="onChangePrice"
+                >
+                    Apply
+                </button>
+            </div>
         </div>
+        <!--    PRICE END-->
     </div>
 </template>
